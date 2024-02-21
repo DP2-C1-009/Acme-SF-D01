8b5bf00afad5a303e302f5ccaf24c9462e7fb960
Ricardo Carreño Mariño <riccarmar@alum.us.es>
2024-02-21 10:04:10 +0100

    Project entity

---
+++ src/main/java/acme/entities/projects/Project.java
@@ -0,0 +1,50 @@
+
+package acme.entities.projects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.datatypes.Money;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Project extends AbstractEntity {
+
+	// Serialisation identifier ---------------------------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -----------------------------------------------------------------------------------------
+
+	@Pattern(regexp = "[A-Z]{3}-[0-9]{4}")
+	@NotBlank
+	@Column(unique = true)
+	private String				code;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				pAbstract;
+
+	private Boolean				hasFatalErrors;
+
+	@Min(value = 0)
+	private Money				cost;
+
+	@URL
+	private String				optionalLink;
+
+}
