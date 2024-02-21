16429d5e87280b2c6a50c7242b794378d232d3ec
jualeomad <jualeomad@alum.us.es>
2024-02-21 12:48:00 +0100

    first version invoice

---
+++ src/main/java/acme/entities/sponsorship/Invoice.java
@@ -0,0 +1,76 @@
+
+package acme.entities.sponsorship;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Invoice extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
+	protected String			code;
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				registrationTime;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				dueDate;
+
+	@Positive
+	protected double			quantity;
+
+	@Min(0)
+	protected double			tax;
+
+	@URL
+	protected String			furtherInfo;
+
+	// Derived attributes -----------------------------------------------------
+
+
+	@Transient
+	public Double totalAmount() {
+		return this.quantity * this.tax;
+	}
+
+	// Relationships ----------------------------------------------------------
+
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Sponsorship sponsorship;
+
+}
