04123cf3cb930ed59f2544df0b986d06f6844725
edubusluc <edubusluc@alum.us.es>
2024-02-26 13:26:38 +0100

    feat: budget restriction

---
+++ src/main/java/acme/entities/contract/Contract.java
@@ -0,0 +1,58 @@
+
+package acme.entities.contract;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.datatypes.Money;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Contract extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "^[A-Z]{1,3}[0-9]{3}$")
+	protected String			code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date				instatationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			providerName;
+
+	@NotBlank
+	@Length(max = 75)
+	protected String			customerName;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String			goals;
+
+	@NotNull
+	protected Money				budget;
+
+}
