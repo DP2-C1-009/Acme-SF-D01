f5f0cf8cbf0b4daa2a7749898c0d376da1e51293
edubusluc <edubusluc@alum.us.es>
2024-03-02 10:08:46 +0100

    feat: add Claim Entity

---
+++ src/main/java/acme/entities/claim/Claim.java
@@ -0,0 +1,67 @@
+
+package acme.entities.claim;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Claim extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "C-[0-9]{4}")
+	@NotNull
+	private String				code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@PastOrPresent
+	@NotNull
+	private Date				instatiationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				heading;
+
+	@NotBlank
+	@Length(max = 100)
+	@NotNull
+	private String				description;
+
+	@NotBlank
+	@Length(max = 100)
+	@NotNull
+	private String				department;
+
+	@Email
+	@NotNull
+	private String				email;
+
+	@URL
+	@Length(max = 255)
+	private String				optionalLink;
+
+}
