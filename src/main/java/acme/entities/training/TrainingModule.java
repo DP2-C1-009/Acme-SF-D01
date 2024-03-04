d816aac1e73481bb97657889d4d814b9c3253160
rafgargal <rafgargal@alum.us.es>
2024-03-04 19:54:18 +0100

    feat: trainingModule entity #46

---
+++ src/main/java/acme/entities/training/TrainingModule.java
@@ -0,0 +1,69 @@
+
+package acme.entities.training;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+
+public class TrainingModule extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	@NotNull
+	protected String			code;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	@Past
+	protected Date				creationMoment;
+
+	@NotBlank
+	@NotNull
+	@Length(max = 100)
+	protected String			details;
+
+	@NotNull
+	protected DifficultyLevel	difficultyLevel;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	protected Date				updateMoment;
+
+	@URL
+	protected String			optionalLink;
+
+	@Positive
+	protected Double			estimatedTotalTime;
+
+	// Relationships ----------------------------------------------------------
+
+	//	@NotNull
+	//	@Valid
+	//	@ManyToOne(optional = false)
+	//	private Project				project;
+	//
+	//	@NotNull
+	//	@Valid
+	//	@ManyToOne(optional = false)
+	//	private Developer			developer;
+
+}
