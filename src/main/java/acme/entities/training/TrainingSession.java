acac8135d21e4e60e6f2d912fe39761d0fcad353
rafgargal <rafgargal@alum.us.es>
2024-03-05 23:08:36 +0100

    fix: codigo descomentado

--- src/main/java/acme/entities/training/TrainingSession.java
+++ src/main/java/acme/entities/training/TrainingSession.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -62,8 +64,8 @@ public class TrainingSession extends AbstractEntity {
 
 	// Relationships ----------------------------------------------------------
 
-	//	@NotNull
-	//	@Valid
-	//	@ManyToOne(optional = false)
-	//	private TrainingModule		trainingModule;
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private TrainingModule		trainingModule;
 }
