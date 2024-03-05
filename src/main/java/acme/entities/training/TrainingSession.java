af062d3a5c69083a2bc80dc171e787171c641cd1
rafgargal <rafgargal@alum.us.es>
2024-03-05 21:15:02 +0100

    feat: training session entity

---
+++ src/main/java/acme/entities/training/TrainingSession.java
@@ -0,0 +1,70 @@
+
+package acme.entities.training;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrainingSession extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Pattern(regexp = "^TS-[A-Z]{1,3}-[0-9]{3}$")
+	@Column(unique = true)
+	@NotNull
+	private String				code;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				startDateTime;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date				endDateTime;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				location;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				instructor;
+
+	@NotBlank
+	@Email
+	private String				contactEmail;
+
+	@URL
+	private String				optionalLink;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	//	@NotNull
+	//	@Valid
+	//	@ManyToOne(optional = false)
+	//	private TrainingModule		trainingModule;
+}
