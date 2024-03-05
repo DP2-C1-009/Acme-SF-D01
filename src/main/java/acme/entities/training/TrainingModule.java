23f70cc44405b76880887104818459176c04df6a
rafgargal <rafgargal@alum.us.es>
2024-03-05 23:10:32 +0100

    fix: codigo descomentado

--- src/main/java/acme/entities/training/TrainingModule.java
+++ src/main/java/acme/entities/training/TrainingModule.java
@@ -5,8 +5,10 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -17,6 +19,8 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.client.data.AbstractEntity;
+import acme.entities.projects.Project;
+import acme.roles.Developer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -60,14 +64,14 @@ public class TrainingModule extends AbstractEntity {
 
 	// Relationships ----------------------------------------------------------
 
-	//	@NotNull
-	//	@Valid
-	//	@ManyToOne(optional = false)
-	//	private Project				project;
-	//
-	//	@NotNull
-	//	@Valid
-	//	@ManyToOne(optional = false)
-	//	private Developer			developer;
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Developer			developer;
 
 }
