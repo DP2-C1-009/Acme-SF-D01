74c4e5c16b817013d16122cc2270f3e8af33f99b
Ricardo Carreño Mariño <riccarmar@alum.us.es>
2024-03-07 21:35:29 +0100

    feat: system configuration about currency

---
+++ src/main/java/acme/entities/systemConfiguration/SystemConfiguration.java
@@ -0,0 +1,31 @@
+
+package acme.entities.systemConfiguration;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SystemConfiguration extends AbstractEntity {
+
+	// Serialisation identifier ---------------------------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -----------------------------------------------------------------------------------------
+
+	@NotBlank
+	@Pattern(regexp = "^[A-Z]{3}$")
+	private String				currency;
+
+	private boolean				systemCurrency;
+
+	private boolean				acceptedCurrency;
+
+}
