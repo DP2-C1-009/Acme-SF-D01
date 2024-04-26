073cacfa2eec539d38a50f7e70c4c43182c69f50
Ricardo Carreño Mariño <riccarmar@alum.us.es>
2024-04-26 20:11:01 +0200

    fix: draft mode internationalization in projects and user stories

--- src/main/java/acme/features/manager/project/ManagerProjectShowService.java
+++ src/main/java/acme/features/manager/project/ManagerProjectShowService.java
@@ -1,6 +1,8 @@
 
 package acme.features.manager.project;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -53,6 +55,14 @@ public class ManagerProjectShowService extends AbstractService<Manager, Project>
 		Dataset dataset;
 
 		dataset = super.unbind(object, "code", "title", "pAbstract", "fatalErrors", "cost", "optionalLink", "draftMode");
+
+		if (object.isDraftMode()) {
+
+			final Locale local = super.getRequest().getLocale();
+			dataset.put("draftMode", local.equals(Locale.ENGLISH) ? "Yes" : "Sí");
+
+		} else
+			dataset.put("draftMode", "No");
 
 		super.getResponse().addData(dataset);
 	}
