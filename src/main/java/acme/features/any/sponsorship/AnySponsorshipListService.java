53bea8c35c026c1d7e6b8244f7ce520a962315bf
jualeomad <jualeomad@alum.us.es>
2024-05-14 20:19:55 +0200

    feat: any sponsorships list/show

---
+++ src/main/java/acme/features/any/sponsorship/AnySponsorshipListService.java
@@ -0,0 +1,52 @@
+
+package acme.features.any.sponsorship;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Any;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.sponsorship.Sponsorship;
+
+@Service
+public class AnySponsorshipListService extends AbstractService<Any, Sponsorship> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AnySponsorshipRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Sponsorship> object;
+
+		object = this.repository.findManyPublishedSponsorships();
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void unbind(final Sponsorship object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "code", "start", "end", "amount", "type");
+
+		dataset.put("project", object.getProject().getCode());
+
+		super.getResponse().addData(dataset);
+	}
+
+}
